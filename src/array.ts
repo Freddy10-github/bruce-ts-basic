317af19e28f6a40b1e356c189266363607d5c1bd
Freddylo <freddy_luo@moneynet.tw>
2022-02-03 20:50:21 +0800

    array and tuple

---
+++ src/array.ts
@@ -0,0 +1,16 @@
+
+let any1: any = 'hello'
+any1 = 999;
+
+let arr1: number[]
+arr1.push(10)
+arr1.push('1')
+
+const arr2: Array<string> = []
+arr2.push('a');
+arr2.push(1);
+
+const arr3: Array<string | boolean> = []
+arr3.push(false)
+arr3.push('b')
+arr3.push(10)
---
+++ src/tuple.ts
@@ -0,0 +1,6 @@
+const tu1: [number, string, boolean] = []
+const tu2: [number, string, boolean] = [99, 'Hi', false]
+
+const tu3: [number, boolean][] = [10, false]
+const tu4: [number, boolean][] = [[10, false], [1, true]]
+
